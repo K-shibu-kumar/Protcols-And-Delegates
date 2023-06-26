12ca6cfc9a6283a59114e289ae9e2984b4a7f508
Dixon A <dixona2023@icloud.com>
2023-06-26 12:23:53 +0530

    Delegate

--- Protcols And Delegates/main.swift
+++ Protcols And Delegates/main.swift
@@ -1,11 +1,53 @@
-//
-//  main.swift
-//  Protcols And Delegates
-//
-//  Created by Dixon A on 25/06/23.
-//
 
-import Foundation
+protocol AdvancedLifeSupport {
+    func performCPR()
+}
 
-print("Hello, World!")
+class EmergencyCallHandler {
+    var delegate: AdvancedLifeSupport?
+    
+    func assessSituation() {
+        print("Can you tell me what happened")
+    }
+    func medicalEmergency() {
+        delegate?.performCPR()
+    }
+}
 
+struct ParaMedic: AdvancedLifeSupport {
+    init(handler: EmergencyCallHandler) {
+        handler.delegate = self
+    }
+    func performCPR() {
+        print("The Paramedic does some chest compressions, 30 per seconds")
+    }
+}
+
+class Doctor: AdvancedLifeSupport {
+    init(handler: EmergencyCallHandler) {
+        handler.delegate = self
+    }
+    
+    func performCPR() {
+        print("The Doctor  does some chest compressions, 30 per seconds")
+    }
+    func useStethoscope() {
+        print("Listening to heartbeat")
+    }
+}
+
+class Surgeon: Doctor {
+    override func performCPR() {
+        super.performCPR()
+        print("Doing some crazy stuff")
+    }
+    func useElectricDrill() {
+        print("Whirr....")
+    }
+}
+
+let emilio = EmergencyCallHandler()
+let dixon = Surgeon(handler: emilio)
+
+emilio.assessSituation()
+emilio.medicalEmergency()
